d7ae4691cddf5dc9e2b3c386147f3d4b50303b30
Camil Blanaru <camil@edka.io>
2017-08-22 20:04:49 +0200

    [WIP] Dump and import using a S3 bucket.

--- Docker/dump.sh
+++ Docker/dump.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+AWS_BUCKET=${AWS_BUCKET:-${MYSQL_ENV_AWS_BUCKET}}
+AWS_BUCKET_PREFIX=${AWS_BUCKET_PREFIX:-${MYSQL_ENV_AWS_BUCKET_PREFIX}}
 DB_USER=${DB_USER:-${MYSQL_ENV_DB_USER}}
 DB_PASS=${DB_PASS:-${MYSQL_ENV_DB_PASS}}
 DB_NAME=${DB_NAME:-${MYSQL_ENV_DB_NAME}}
@@ -7,6 +12,16 @@ DB_HOST=${DB_HOST:-${MYSQL_ENV_DB_HOST}}
 ALL_DATABASES=${ALL_DATABASES}
 IGNORE_DATABASE=${IGNORE_DATABASE}
 
+
+if [ "${AWS_BUCKET}" == "" ]; then
+  echo "Missing AWS_BUCKET env variable"
+  exit 1
+fi
+
+if [ "${AWS_BUCKET_PREFIX}" == "" ]; then
+  echo "Missing AWS_BUCKET_PREFIX env variable"
+  exit 1
+fi
 
 if [[ ${DB_USER} == "" ]]; then
 	echo "Missing DB_USER env variable"
@@ -26,13 +41,13 @@ if [[ ${ALL_DATABASES} == "" ]]; then
 		echo "Missing DB_NAME env variable"
 		exit 1
 	fi
-	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > /mysqldump/"${DB_NAME}".sql
+	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" | gzip | aws s3 cp - s3://$AWS_BUCKET/$AWS_BUCKET_PREFIX/$(date +"%Y")/$(date +"%m")/$(date +"%d")/"${DB_NAME}".sql.gz
 else
 	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
         echo "Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/$db.sql
+        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db | gzip | aws s3 cp - s3://$AWS_BUCKET/$AWS_BUCKET_PREFIX/$(date +"%Y")/$(date +"%m")/$(date +"%d")/$db.sql.gz
     fi
 done
 fi
